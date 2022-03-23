13661fd6d6ce0b4bf0ff50b9fd073fb74d048f8d
Kyâne <kyane@kyane.fr>
2022-03-23 19:11:13 +0100

    Add Zalex theme

--- photobooth/gui/Qt5Gui/__init__.py
+++ photobooth/gui/Qt5Gui/__init__.py
@@ -22,6 +22,7 @@ styles = (('default', 'stylesheets/default.qss'),
           ('dark (1024 x 600 px)', 'stylesheets/dark-1024x600.qss'),
           ('dark (800 x 600 px)', 'stylesheets/dark-800x600.qss'),
           ('pastel (1024 x 600 px)', 'stylesheets/pastel-1024x600.qss'),
-          ('pastel (800 x 600 px)', 'stylesheets/pastel-800x600.qss'))
+          ('pastel (800 x 600 px)', 'stylesheets/pastel-800x600.qss'),
+          ('zalex (1280 x 720 px)', 'stylesheets/zalex-1280x720.qss'))
 
 from .PyQt5Gui import PyQt5Gui  # noqa
